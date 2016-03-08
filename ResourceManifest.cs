a50fe3da19c2b1f2a895c81782cf4e6d6ef37ae3
Csanad Balla <csanad.balla@lombiq.com>
2016-03-08 16:32:55 +0100

    Adding slick to the project
    --HG--
    branch : issue/NEST-168

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -9,6 +9,10 @@ namespace MediaKitty.Extensions
             var manifest = builder.Add();
 
             manifest.DefineStyle("MediaLibraryUploadField").SetUrl("media-library-upload-field.min.css", "media-library-upload-field.css");
+
+            manifest.DefineStyle("SlickTheme").SetUrl("slick-theme.css");
+            manifest.DefineStyle("Slick").SetUrl("slick.css");
+            manifest.DefineScript("Slick").SetUrl("slick.min.js", "slick.js");
         }
     }
 }

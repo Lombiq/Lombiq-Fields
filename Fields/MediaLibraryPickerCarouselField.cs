5059d3dcacce7be843e9d56572e6c6be0cdf5089
Csanad Balla <csanad.balla@lombiq.com>
2016-03-20 23:24:09 +0100

    Adding CarouselField and Handler
    --HG--
    branch : issue/NEST-168

---
+++ Fields/MediaLibraryPickerCarouselField.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Lombiq.Fields.Settings;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.FieldStorage;
+using Orchard.Environment.Extensions;
+using Orchard.MediaLibrary.Fields;
+
+namespace Lombiq.Fields.Fields
+{
+    [OrchardFeature("Lombiq.Fields.MediaLibraryPickerFieldCarousel")]
+    public class MediaLibraryPickerCarouselField : ContentField
+    {
+        private static readonly char[] _separators = new[] { '{', '}', ',' };
+        public MediaLibraryPickerFieldCarouselSettings Settings
+        {
+            get { return DecodeSettings(Storage.Get<string>()); }
+            set { Storage.Set(EncodeSettings(value)); }
+        }
+
+        private string EncodeSettings(MediaLibraryPickerFieldCarouselSettings settings)
+        {
+            if (settings == null)
+            {
+                return string.Empty;
+            }
+
+            return "{" + settings.IsCarousel.ToString() + "},{"
+                + settings.IsSingleItem.ToString() + "},{"
+                + settings.IsInfinite.ToString() + "},{"
+                + settings.ItemsToShow.ToString() + "},{"
+                + settings.ItemsToScroll.ToString() + "},{"
+                + settings.IsAutoplay.ToString() + "},{"
+                + settings.AutoplaySpeed.ToString() + "}";
+        }
+
+        private MediaLibraryPickerFieldCarouselSettings DecodeSettings(string settings)
+        {
+            if (String.IsNullOrWhiteSpace(settings))
+            {
+                return null;
+            }
+
+            var storedSettings = settings.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+            var carouselSettings = new MediaLibraryPickerFieldCarouselSettings();
+
+            carouselSettings.IsCarousel = bool.Parse(storedSettings[0]);
+            carouselSettings.IsSingleItem = bool.Parse(storedSettings[1]);
+            carouselSettings.IsInfinite = bool.Parse(storedSettings[2]);
+            carouselSettings.ItemsToShow = int.Parse(storedSettings[3]);
+            carouselSettings.ItemsToScroll = int.Parse(storedSettings[4]);
+            carouselSettings.IsAutoplay = bool.Parse(storedSettings[5]);
+            carouselSettings.AutoplaySpeed = int.Parse(storedSettings[6]);
+
+            return carouselSettings;
+        }
+    }
+}
---
+++ Handlers/MediaLibraryPickerFieldCarouselHandler.cs
@@ -0,0 +1,41 @@
+﻿using Lombiq.Fields.Fields;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Environment.Extensions;
+using Orchard.MediaLibrary.Fields;
+using System.Linq;
+
+namespace Lombiq.Fields.Handlers
+{
+    [OrchardFeature("Lombiq.Fields.MediaLibraryPickerFieldCarousel")]
+    public class MediaLibraryPickerFieldCarouselHandler : ContentHandler
+    {
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+
+        public MediaLibraryPickerFieldCarouselHandler(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+        protected override void Initializing(InitializingContentContext context)
+        {
+            base.Initializing(context);
+
+            var partsWithMediaLibraryPickerField = context.ContentItem.Parts.SelectMany(x => x.Fields.OfType<MediaLibraryPickerField>());
+            if (partsWithMediaLibraryPickerField.Any())
+            {
+                var partsWithCarouselField = context.ContentItem.Parts.SelectMany(x => x.Fields.OfType<MediaLibraryPickerCarouselField>());
+                if (!partsWithCarouselField.Any())
+                {
+                    _contentDefinitionManager.AlterPartDefinition(context.ContentItem.Parts.Where(
+                        x => x.PartDefinition.Name == context.ContentItem.ContentType).FirstOrDefault()
+                        .TypePartDefinition.PartDefinition.Name,
+                        cfg => cfg.WithField(typeof(MediaLibraryPickerCarouselField).Name,
+                            f => f.OfType(typeof(MediaLibraryPickerCarouselField).Name)
+                                .WithDisplayName(typeof(MediaLibraryPickerCarouselField).Name)));
+                }
+            }
+        }
+    }
+}

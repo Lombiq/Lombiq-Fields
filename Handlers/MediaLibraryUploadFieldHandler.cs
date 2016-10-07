3f3e6111c86e74f1e069187a6e583470e6bfa202
Balázs Tóth <balazs.toth@lombiq.com>
2016-10-07 11:10:23 +0200

    Implementing the IFormElementEventHandler interface
    --HG--
    branch : issue/OSOE-1

--- Handlers/MediaLibraryUploadFieldHandler.cs
+++ Handlers/MediaLibraryUploadFieldHandler.cs
@@ -5,20 +5,27 @@ using Orchard.ContentManagement.Handlers;
 using Orchard.ContentManagement.MetaData;
 using Orchard.Environment.Extensions;
 using Orchard.MediaLibrary.Models;
+using Orchard.DynamicForms.Services;
+using MediaLibraryUploadField = Lombiq.Fields.Fields.MediaLibraryUploadField;
+using MediaLibraryUploadFieldElement = Orchard.DynamicForms.Elements.MediaLibraryUploadField;
+using Orchard.DynamicForms.Elements;
+using Orchard.DynamicForms.Services.Models;
+using Orchard;
 
 namespace Lombiq.Fields.Handlers
 {
     [OrchardFeature("Lombiq.Fields.MediaLibraryUploadField")]
-    public class MediaLibraryUploadFieldHandler : ContentHandler
+    public class MediaLibraryUploadFieldHandler : ContentHandler, IFormElementEventHandler
     {
         private readonly IContentManager _contentManager;
         private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly IWorkContextAccessor _wca;
 
-
-        public MediaLibraryUploadFieldHandler(IContentManager contentManager, IContentDefinitionManager contentDefinitionManager)
+        public MediaLibraryUploadFieldHandler(IContentManager contentManager, IContentDefinitionManager contentDefinitionManager, IWorkContextAccessor wca)
         {
             _contentManager = contentManager;
             _contentDefinitionManager = contentDefinitionManager;
+            _wca = wca;
         }
 
 
@@ -32,5 +39,19 @@ namespace Lombiq.Fields.Handlers
 
             foreach (var field in fields) field.MediaPartsField.Loader(() => _contentManager.GetMany<MediaPart>(field.Ids, VersionOptions.Published, QueryHints.Empty).ToList());
         }
+
+        public void GetElementValue(FormElement element, ReadElementValuesContext context)
+        {
+            var uploadField = element as MediaLibraryUploadFieldElement;
+
+            if (uploadField == null)
+                return;
+
+           // var key = uploadField.Name;
+           //// var fileName = context.ValueProvider;
+           // context.Output[key] = currentUser != null ? currentUser.UserName : null;
+        }
+
+        public void RegisterClientValidation(FormElement element, RegisterClientValidationAttributesContext context) { }
     }
 }

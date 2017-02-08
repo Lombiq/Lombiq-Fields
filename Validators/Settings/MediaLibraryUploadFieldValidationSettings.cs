fccac82e0d7898a830ec8d28a6890fab5600ef13
Albert Hajdu <albert.hajdu@lombiq.com>
2017-02-08 10:38:35 +0100

    Reverting all files to a6f376773c4f
    --HG--
    branch : issue/OSOE-1

--- Validators/Settings/MediaLibraryUploadFieldValidationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using Orchard.DynamicForms.Services.Models;
-
-namespace Orchard.DynamicForms.Validators.Settings
-{
-    public class MediaLibraryUploadFieldValidationSettings : ValidationSettingsBase
-    {
-        public bool? IsRequired { get; set; }
-    }
-}

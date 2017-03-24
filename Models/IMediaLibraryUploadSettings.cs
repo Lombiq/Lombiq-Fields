6d8522e2617f001428e1582beccd79f1a658fc4a
Albert Hajdu <albert.hajdu@lombiq.com>
2017-03-24 19:21:40 +0100

    Adding documentation
    --HG--
    branch : issue/OSOE-1

--- Models/IMediaLibraryUploadSettings.cs
+++ Models/IMediaLibraryUploadSettings.cs
@@ -5,15 +5,42 @@ using System.Web;
 
 namespace Lombiq.Fields.Models
 {
+    /// <summary>
+    /// The common settings between the field and the element.
+    /// </summary>
     public interface IMediaLibraryUploadSettings
     {
+        /// <summary>
+        /// A comma-separated list of the file extensions that are allowed to be uploaded. Leaving it empty will make the field to accept all file types.
+        /// </summary>
         string AllowedExtensions { get; }
+        /// <summary>
+        /// The maximum file size per uploaded file in kilobytes. 0 means no limit.
+        /// </summary>
         int MaximumSizeKB { get; }
+        /// <summary>
+        /// The maximum width for uploaded images in pixels. 0 means no limit.
+        /// </summary>
         int ImageMaximumWidth { get; }
+        /// <summary>
+        /// The maximum height for uploaded images in pixels. 0 means no limit.
+        /// </summary>
         int ImageMaximumHeight { get; }
+        /// <summary>
+        /// Whether allowed to the user to upload multiple items.
+        /// </summary>
         bool Multiple { get; }
+        /// <summary>
+        /// The maximum size of all uploaded files for this field in megabytes. 0 means no limit.
+        /// </summary>
         int FieldStorageUserQuotaMB { get; }
+        /// <summary>
+        /// Whether uploading at least one item is required.
+        /// </summary>
         bool Required { get; }
+        /// <summary>
+        /// The hint text of the field.
+        /// </summary>
         string Hint { get; }
     }
 }
